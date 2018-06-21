32e6e4c5abacff27714218c8df49a42e8d6d6fa5
Harish Bhakuni <harishbhakuni21@gmail.com>
2018-06-21 12:53:55 +0530

    correction in settings

--- src/muypicky/settings/base.py
+++ src/muypicky/settings/base.py
@@ -48,8 +48,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'menus',
     'restaurants',
+    'menus',
+    'profiles',
 ]
 
 MIDDLEWARE = [
